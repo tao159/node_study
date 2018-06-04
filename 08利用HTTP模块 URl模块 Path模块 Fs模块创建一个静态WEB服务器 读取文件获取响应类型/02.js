1e4a56d029ecaee1a440de3fb8acb9e661a419d0
tao159 <yangtao155@163.com>
2018-06-04 18:13:55 +0800

    message

--- 08利用HTTP模块 URl模块 Path模块 Fs模块创建一个静态WEB服务器 读取文件获取响应类型/02.js
+++ 08利用HTTP模块 URl模块 Path模块 Fs模块创建一个静态WEB服务器 读取文件获取响应类型/02.js
@@ -1,13 +1,16 @@
 var http = require('http');
-fs = require('fs');
+var fs = require('fs');
+var path = require('path');
 http.createServer(function(req, res) {
+
     var pathname = req.url;
     if (pathname == '/') {
-        pathname = '/index.html';
+        pathname = '/index.html'
     }
     if (pathname != '/favicon.ico') {
         fs.readFile('static/' + pathname, function(err, data) {
             if (err) {
+                console.log(err);
                 fs.readFile('static/404.html', function(err_404, data_404) {
                     res.writeHead(404, { 'Content-type': 'text/html;charset:utf-8' });
                     res.write(data_404);
@@ -16,9 +19,12 @@ http.createServer(function(req, res) {
             } else {
                 res.writeHead(200, { 'Content-type': 'text/html;charset:utf-8' });
                 res.write(data);
+                console.log(path);
                 res.end();
             }
-        });
+        })
     }
 }).listen(3000);
-console.log('server running at 127.0.0.1:3000')
+console.log('*****************************');
+console.log('server running at "127.0.0.1:3000');
+console.log('*****************************');
